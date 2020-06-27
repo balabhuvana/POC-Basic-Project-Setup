cb76de54734b32eb570f7409a621a326c10a1384
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-06-27 13:08:47 +0530

    Initial commit

---
+++ app/src/main/java/com/arunv/poc_basic_project_setup/SingleActivity.kt
@@ -0,0 +1,12 @@
+package com.arunv.poc_basic_project_setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+    }
+}
